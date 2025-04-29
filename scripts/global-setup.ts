084768502eed6b8159d6556ce44904d5b869137f
DoreyKiss <dorey.kiss@gmail.com>
2025-04-29 13:11:30 +0200

    prisma-01

--- scripts/global-setup.ts
+++ scripts/global-setup.ts
@@ -2,5 +2,5 @@ import { truncateTables } from "./truncate-tables";
 
 export default async function globalSetup(): Promise<void> {
   console.log('Running global setup once before everything');
- await truncateTables();
+  await truncateTables();
 }
